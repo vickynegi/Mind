dbde377ae5eebdd39d7bf2788b79226e0d7c5ec4
vickynegi <v_111negi@yahoo.com>
2019-06-26 23:13:17 +0530

    given permission

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,12 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?  
+  protect_from_forgery with: :exception
   
+  def after_sign_in_path_for(resource)
+    current_artist
+  end
   protected
-
+ 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password, :username, :country, :city, :phone, :photo)}
     devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :email, :password, :current_password, :username, :country, :city, :phone,  :photo )}
--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,7 @@ Rails.application.routes.draw do
   	  patch 'save_photo'
       patch 'save_paintings'
   	end
-    #resources :paintings
   end
   root to: 'artists#index'
-  match '/signup', to: 'artists#index', via: 'get'
-  match '/signin', to: 'artists#show', via: 'get'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
